8f0a163c7eea710a1fcc50310efe6dda4f182968
Hai Ng <nguyenhai0527@gmail.com>
2024-08-11 20:00:20 +0700

    view: Humindity Chart

--- dashboard/app/page.tsx
+++ dashboard/app/page.tsx
@@ -1,3 +1,5 @@
+import Actions from "@/components/actions";
+import Humidity from "@/components/humidity-chart";
 import MotionCard from "@/components/motion-card";
 import { TempChart } from "@/components/temp-chart";
 
@@ -6,10 +8,12 @@ export default function Home() {
     <main className="">
       <div className="min-h-screen p-8">
         <h1 className="text-3xl font-bold mb-8">IoT Monitoring Dashboard</h1>
-        <div className="grid grid-cols-1 gap-8">
-          <MotionCard title="Performance" children={<TempChart />}>
-            {/* Chart component for Performance */}
-          </MotionCard>
+        <div className="grid grid-cols-1 gap-8 mb-8">
+          <MotionCard title="DHT11/DHT12" children={<TempChart />} />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <MotionCard title="Humidity" children={<Humidity />} />
+          <MotionCard title="Actions" children={<Actions />} />
         </div>
       </div>
     </main>
---
+++ dashboard/components/actions.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Actions = () => {
+  return <div>Actions</div>;
+};
+
+export default Actions;
