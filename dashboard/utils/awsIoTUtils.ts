89449a8e62d79ac591590b501abf5d8d566f7436
Hai Ng. <105899937+nguyenhai0527@users.noreply.github.com>
2024-08-21 09:07:26 +0700

    Update awsIoTUtils.ts

--- dashboard/utils/awsIoTUtils.ts
+++ dashboard/utils/awsIoTUtils.ts
@@ -2,10 +2,10 @@ import AWSIoT from "aws-iot-device-sdk";
 
 export function createIoTDevice() {
   // Replace these with your actual AWS IoT credentials and endpoint
-  const region = "ap-southeast-1";
-  const host = "a1kfygo2s7d0ba-ats.iot.ap-southeast-1.amazonaws.com"; // Your IoT endpoint
-  const accessKeyId = "AKIATZQUBAUZI23ABI74";
-  const secretAccessKey = "xhIaUvTaRHw41zfRg2g1DY0fkg5J0CoJz6m3NZHU";
+  const region = "";
+  const host = ""; // Your IoT endpoint
+  const accessKeyId = "";
+  const secretAccessKey = "";
 
   return new AWSIoT.device({
     region,
